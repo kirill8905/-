b32cf790dee6b2aa6fb3227f0ba783a6f825cd7d
kirill8905 <liners485@gmail.com>
2022-12-10 21:26:02 +0700

    Inital Commit

--- matrix.c
+++ matrix.c
@@ -1,7 +1,23 @@
 #include <stdio.h>
 
-#define N 10
-#define M 10
+#define N 3
+#define M 3
 
-char str [N][M];
-int a = 
+
+int main()
+{
+   int arr[N][M];
+   int i, j, a;
+   a = 1;
+
+        for(i = 0; i < N; i++)
+           { 
+		for(j = 0, a; j < M; j++, a++)
+   		   {
+			arr[i][j] = a;
+			printf(" %d ", arr[i][j]);
+		   }
+		printf("\n");
+   	   }
+}    
+
